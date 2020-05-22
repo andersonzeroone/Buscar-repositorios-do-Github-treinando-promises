45c2e57f8fc0bd246c5c08573745629e496ecc02
P <anderson.pablo02@gmail.ccom>
2020-05-22 16:03:13 -0300

    up

--- promises.js
+++ promises.js
@@ -1,16 +1,13 @@
-function usersRepos() {
+function usersRepos() { //andersonzeroone
     return new Promise(function(resolve, reject){
     
     let user = document.querySelector('.input').value;
+    // let show = document.querySelector('.user');
+    // let showUser = document.querySelector('p')
+    // showUser.innerHTML = `Usuario: ${user}` 
+    // show.appendChild(showUser);
+
     let ul = document.querySelector('ul');
-
-  
-    let show = document.querySelector('.user');
-    let showUser = document.createElement('p')
-    showUser.innerHTML = `Usuario: ${user}` 
-
-    show.appendChild(showUser);
-    
     console.log(user);
 
     let xhr = new XMLHttpRequest();
@@ -20,7 +17,6 @@ function usersRepos() {
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
                 if(xhr.status === 200){
-                  
                     let repo =  JSON.parse(xhr.responseText)  
                     console.log(` Repositorios ${repo}`)
                     
@@ -31,11 +27,11 @@ function usersRepos() {
                     
                     for(let i=0; i < repoList.length; i++ ){
                         let li = document.createElement('li');
-                        li.innerHTML = `Repositorio ${num +1}: ${repoList[i]}}`
+                        li.innerHTML = `Repositorio ${num +1}: ${repoList[i]}`
                         ul.appendChild(li);
                          num++;
                     }
-  
+                    document.querySelector(".input").value = ''
                 }else{
                     reject('erro');
                 }
@@ -45,16 +41,18 @@ function usersRepos() {
 }
 
 function usersPromises(){
+    let user = document.querySelector('.input').value;
 
-
-    
-    usersRepos()
-        .then( function(response){
-            console.log(response);
-           
-        })
-        .catch( function (erros){
-            console.log(error);
-        })
+    user == '' ? (alert('Preencha o Campo!')): (
+            usersRepos()
+            .then( function(response){
+                console.log(response);
+                
+            
+            })
+            .catch( function (erros){
+                console.log(error);
+            })
+    )    
 }
 
