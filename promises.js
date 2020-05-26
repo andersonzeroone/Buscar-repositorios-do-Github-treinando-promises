7e7110d5b67cd32b88beb579b43cb325642b0b57
P <anderson.pablo02@gmail.ccom>
2020-05-25 23:10:14 -0300

    promises function

--- promises.js
+++ promises.js
@@ -1,31 +1,36 @@
 function usersPromises(){
     let user = document.querySelector('.input').value; 
-    let ul = document.querySelector('ul');   
+    let ul = document.querySelector('ul'); 
+    let urlUser = `https://api.github.com/users/${user}`; 
+    let urlRepos =  `https://api.github.com/users/${user}/repos`;     
     
     user == '' ? (alert('Preencha o Campo!')):
         (
-            ul == null ?(usersRepos()
+            ul == null ?(
+                usersRepos(urlRepos)
+                        .then((response) =>{
+                            console.log(` repo ${response}`);
+                            render(response);
+                        })
+                        .catch((erros) =>{ console.warn(erros),alert(erros)})
             ):
             (
-                usersRepos(),
+                usersRepos(urlRepos)
+                    .then((response) =>{
+                        console.log(` repo ${response}`);
+                        render(response);
+                    })
+                    .catch((erros) =>{ console.warn(erros),alert(erros)}),
                 document.getElementById('ulList').remove(),
                 document.getElementById('name').remove()
             ) 
         )    
 }
 
-function usersRepos() { //andersonzeroone
-    return new Promise(function(resolve, reject){
-
+const usersRepos = (url) => new Promise((resolve, reject) =>{
     setLoading(true);
-
-    let user = document.querySelector('.input').value;
-    let urlUser = `https://api.github.com/users/${user}`; 
-    let urlRepos =  `https://api.github.com/users/${user}/repos`;   
-    console.log(user);
-
     let xhr = new XMLHttpRequest();
-    xhr.open('GET',`${urlRepos}`);
+    xhr.open('GET',`${url}`);
     xhr.send(null);
 
         xhr.onreadystatechange = function(){
@@ -33,7 +38,6 @@ function usersRepos() { //andersonzeroone
                 if(xhr.status === 200){
 
                     setLoading(false);
-
                     resolve(JSON.parse(xhr.responseText));
                     
                 }else{
@@ -43,11 +47,33 @@ function usersRepos() { //andersonzeroone
                 }
             }
         }
-    })
-    .then((response) =>{
-        console.log(` repo ${response}`);
+}) 
 
-        let list = [];
+
+function setLoading(loading){
+    let user = document.querySelector('.input').value;
+    let show = document.querySelector('.user');
+    let showUser = document.createElement('p');
+    showUser.setAttribute('id','name');
+
+    let loadingEl = document.querySelector('.load');
+    let menssenger = document.createElement('span') ;
+   
+    loading == true?(
+            menssenger.setAttribute('id', 'loading'),
+            menssenger.innerHTML = 'loading...',  
+            loadingEl.appendChild(menssenger)
+        )
+        :(
+            document.getElementById('loading').remove(),
+            showUser.innerHTML = `Usuario: ${user}`,
+            show.appendChild(showUser)
+        )
+              
+}
+
+function render(response){
+    let list = [];
 
         let ul = document.createElement('ul');
         ul.setAttribute('id','ulList');
@@ -81,29 +107,4 @@ function usersRepos() { //andersonzeroone
         })
 
         document.querySelector(".input").value = ''
-    })
-    .catch((erros) =>{ console.warn(erros),alert(erros)})
-}
-
-
-function setLoading(loading){
-    let user = document.querySelector('.input').value;
-    let show = document.querySelector('.user');
-    let showUser = document.createElement('p');
-    showUser.setAttribute('id','name');
-
-    let loadingEl = document.querySelector('.load');
-    let menssenger = document.createElement('span') ;
-   
-    loading == true?(
-            menssenger.setAttribute('id', 'loading'),
-            menssenger.innerHTML = 'loading...',  
-            loadingEl.appendChild(menssenger)
-        )
-        :(
-            document.getElementById('loading').remove(),
-            showUser.innerHTML = `Usuario: ${user}`,
-            show.appendChild(showUser)
-        )
-              
 }
